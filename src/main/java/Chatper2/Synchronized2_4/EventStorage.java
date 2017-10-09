b2a7df9f0e7778bed0ee8a4ec2f0a4310188ab01
zhangke <294667739@qq.com>
2017-10-10 00:34:52 +0800

    20171010

--- src/main/java/Chatper2/Synchronized2_4/EventStorage.java
+++ src/main/java/Chatper2/Synchronized2_4/EventStorage.java
@@ -9,32 +9,37 @@ public class EventStorage {
 	private List<Date> storage;
 
 	public EventStorage() {
-		maxSize = 10;
+		maxSize = 100;
 		storage = new LinkedList<Date>();
 	}
 
 	public synchronized void set() {
 		while (storage.size() == maxSize) {
 			try {
+				System.out.println("共享池已满，生产者开始wait");
 				wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
+		System.out.println("生产者生产了，唤醒消费者消费");
 		storage.add(new Date());
-		System.out.printf("Set: %d", storage.size());
+		
+		System.out.printf("Set: %d \n", storage.size());
 		notifyAll();
 	}
 
 	public synchronized void get() {
 		while (storage.size() == 0) {
 			try {
+				System.out.println("共享池已空，消费者开始wait");
 				wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-		System.out.printf("Get: %d: %s", storage.size(), ((LinkedList<?>) storage).poll());
+		System.out.println("消费者消费了，唤醒生产者继续生产");
+		System.out.printf("Get: %d: %s \n", storage.size(), ((LinkedList<?>) storage).poll());
 		notifyAll();
 	}
 
